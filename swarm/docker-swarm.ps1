d59a6e427d357132d2b4e7b5a242d8803fabc273
Leandro Scardua <leandroscar@gmail.com>
2020-09-01 16:14:19 -0400

    Update docker-swarm.ps1

--- swarm/docker-swarm.ps1
+++ swarm/docker-swarm.ps1
@@ -1,10 +1,9 @@
-remove network interface
-
-get-netad** - Check if virtual network exist, if yes, run the followiing command.
 
 
-stop-service hns
-stop-service docker
-del 'C:\ProgramData\Microsoft\Windows\hns\hns.data'
-start-service hns
-start-service docker
+
+New-Item -Path "C:\ProgramData\Docker\config\daemon.json" -force
+Set-Content -Path "C:\ProgramData\Docker\config\daemon.json" -Value '
+{
+    "bridge" : "none"
+}
+'
